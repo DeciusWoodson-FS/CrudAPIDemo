5ed8733aefa2c53ed2bf17393fb3e1d75bba9af3
DeciusWoodson-FS <wsdecius@student.fullsail.edu>
2025-12-02 19:07:43 -0500

    updated server.js

--- server.js
+++ server.js
@@ -12,15 +12,15 @@ const studentRouter = require("./routes/students");
 
 const DATABASE_URL = process.env.DATABASE_URL;
 
-app.use(cors());
-app.use(express.json());
-app.use("/students", studentRouter);
-
 mongoose.connect(DATABASE_URL);
 
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Database Connection Established"));
+
+app.use(cors());
+app.use(express.json());
+app.use("/students", studentRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
